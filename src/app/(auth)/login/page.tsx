baadcb42e5229790c5a8f147fd3c811a20c1e7f4
koustabnandy <kkoustabnnandy@gmail.com>
2026-07-18 20:41:03 +0530

    Remove unused empty files

--- src/app/(auth)/login/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function LoginPage() {
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Login Page</h1>
-      <p>Coming Soon...</p>
-    </div>
-  );
-}
--- src/app/(auth)/signup/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SignupPage() {
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Signup Page</h1>
-      <p>Coming Soon...</p>
-    </div>
-  );
-}
